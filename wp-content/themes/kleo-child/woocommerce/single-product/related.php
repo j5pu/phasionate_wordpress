7c355ab332655835312021ce52c5e4d005c8f636
yeray-bizeulabs <yeray@bizeulabs.com>
2015-08-24 16:18:50 +0200

    TIENDA: bug fix related products

--- wp-content/themes/kleo-child/woocommerce/single-product/related.php
+++ wp-content/themes/kleo-child/woocommerce/single-product/related.php
@@ -11,49 +11,42 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $product, $woocommerce_loop;
 
+
 $related = $product->get_related( $posts_per_page );
 
 if ( sizeof( $related ) == 0 ) return;
 
-$args = apply_filters( 'woocommerce_related_products_args', array(
-	'post_type'				=> 'product',
-	'ignore_sticky_posts'	=> 1,
-	'no_found_rows' 		=> 1,
-	'posts_per_page' 		=> $posts_per_page,
-	'orderby' 				=> $orderby,
-	'post__in' 				=> $related,
-	'post__not_in'			=> array( $product->id )
-) );
-
-$products = new WP_Query( $args );
 
 $woocommerce_loop['columns'] = $columns;
 
 $items = sq_option( 'woo_related_columns', 3 );
 
-$subheadingvalues = get_the_terms( $product->id, 'pa_coleccion');
+$coleccion = get_the_terms( $product->id, 'pa_coleccion');
 
-if ( $subheadingvalues && ! is_wp_error( $subheadingvalues ) ) {
+$args = apply_filters( 'woocommerce_related_products_args', array(
+	'post_type'           => 'product',
+	'post_status'         => 'publish',
+	'ignore_sticky_posts' => 1,
+	'posts_per_page'      => 3,
+	'meta_query'          => WC()->query->get_meta_query(),
+	'post__not_in'			=> array( $product->id ),
+	'tax_query'           => array(
+		array(
+			'taxonomy' => $coleccion[0]->taxonomy,
+			'terms'    => array_map( 'sanitize_title', explode( ',', $coleccion[0]->slug ) ),
+			'field'    => 'slug'
+		)
+	)
+) );
+
+$products = new WP_Query( $args );
+
+if ( $coleccion && ! is_wp_error( $coleccion ) ) {
 	?>
 
 	<div class="related products kleo-shop-<?php echo $items;?>">
 
 		<div class="hr-title hr-full"><abbr><?php _e( 'Related Products', 'woocommerce' ); ?></abbr></div>
-	<?php
-	foreach ( $subheadingvalues as $subheadingvalue ) {
-		echo do_shortcode('[product_attribute attribute="coleccion" filter="'.$subheadingvalue->slug.'" columns="3" per_page="3"]');
-	}		
-	?>
-	</div>
-	<?php
-}else{
-
-if ( $products->have_posts() ) : ?>
-
-	<div class="related products kleo-shop-<?php echo $items;?>">
-
-		<div class="hr-title hr-full"><abbr><?php _e( 'Related Products', 'woocommerce' ); ?></abbr></div>
-
 		<?php woocommerce_product_loop_start(); ?>
 
 			<?php while ( $products->have_posts() ) : $products->the_post(); ?>
@@ -62,12 +55,10 @@ if ( $products->have_posts() ) : ?>
 
 			<?php endwhile; // end of the loop. ?>
 
-		<?php woocommerce_product_loop_end(); ?>
-
+			<?php woocommerce_product_loop_end(); ?>
+		?>
 	</div>
-
-<?php endif;
-
+	<?php
 }
 
 wp_reset_postdata();
