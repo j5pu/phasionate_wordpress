a82532c3500bfa850158a4cac87bcd37c08abf9c
lobato02 <pablo@mnopi.com>
2015-08-27 21:20:12 +0200

    Eliminados los javascript de los botones de follow de las redes sociales en el footer

--- wp-content/themes/kleo-child/footer.php
+++ wp-content/themes/kleo-child/footer.php
@@ -38,19 +38,3 @@
 	<?php wp_footer(); ?>
 	
 </body>
-<!-- Script Facebook -->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&appId=783363955070748&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<!-- Script Twitter -->
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-<!-- Script Pinterest -->
-<script type="text/javascript" async defer src="//assets.pinterest.com/js/pinit.js"></script>
-<!-- Script Instagram -->
-<script>(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src="//x.instagramfollowbutton.com/follow.js";s.parentNode.insertBefore(g,s);}(document,"script"));</script>
-</html>
