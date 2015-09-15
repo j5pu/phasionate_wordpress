f9ed583ed40715d5b0e620cf0773df70ccfd6241
lobato02 <pablo@mnopi.com>
2015-09-15 12:11:12 +0200

    Resuelve problema de paginación

--- wp-content/themes/kleo-child/functions.php
+++ wp-content/themes/kleo-child/functions.php
@@ -166,33 +166,7 @@ function kleo_title()
 
 }
 
-/*
-*
-* Elminar permalink author
-*
-*/
 
-// The first part //  - He quitado el page de la paginacion 
-add_filter('author_rewrite_rules', 'no_author_base_rewrite_rules');
-function no_author_base_rewrite_rules($author_rewrite) {
-    global $wpdb;
-    $author_rewrite = array();
-    $authors = $wpdb->get_results("SELECT user_nicename AS nicename from $wpdb->users");   
-    foreach($authors as $author) {
-        //$author_rewrite["({$author->nicename})/page/?([0-9]+)/?$"] = 'index.php?author_name=$matches[1]&paged=$matches[2]';
-		$author_rewrite["({$author->nicename})/?([0-9]+)/?$"] = 'index.php?author_name=$matches[1]&paged=$matches[2]';
-        $author_rewrite["({$author->nicename})/?$"] = 'index.php?author_name=$matches[1]';
-    }  
-    return $author_rewrite;
-}
- 
-// The second part //
-add_filter('author_link', 'no_author_base', 1000, 2);
-function no_author_base($link, $author_id) {
-    $link_base = trailingslashit(get_option('home'));
-    $link = preg_replace("|^{$link_base}author/|", '', $link);
-    return $link_base . $link;
-}
 
 
 /*
