dde791bc6bbca9bc44fb22dd936e9cbbf65a007c
lobato02 <pablo@mnopi.com>
2015-10-15 14:12:07 +0200

    Cambio de codigo de los banners laterales. Código de zanox sin pasar por adspeed

--- wp-content/themes/kleo-child/page-parts/general-after-wrap.php
+++ wp-content/themes/kleo-child/page-parts/general-after-wrap.php
@@ -28,9 +28,10 @@ $container = apply_filters('kleo_main_container_class','container');
 		</div><!--end .container-->
   	<!-- AdSpeed.com Tag 8.0.2 for [Ad] Swarovski - Laterales 120x600 -->
 	<div style="float: right; margin-right: 20px; width: 121px;">
-	<script type="text/javascript">var asdate=new Date();var q='&tz='+asdate.getTimezoneOffset()/60 +'&ck='+(navigator.cookieEnabled?'Y':'N') +'&jv='+(navigator.javaEnabled()?'Y':'N') +'&scr='+screen.width+'x'+screen.height+'x'+screen.colorDepth +'&z='+Math.random() +'&ref='+escape(document.referrer.substr(0,255)) +'&uri='+escape(document.URL.substr(0,255));document.write('<ifr'+'ame width="121" height="600" src="http://g.adspeed.net/ad.php?do=html&aid=243394&oid=19457&wd=121&ht=600&target=_blank'+q+'" frameborder="0" scrolling="no" allowtransparency="true" hspace="0" vspace="0"></ifr'+'ame>');</script>
-	<noscript><iframe width="121" height="600" src="http://g.adspeed.net/ad.php?do=html&aid=243394&oid=19457&wd=121&ht=600&target=_blank" frameborder="0" scrolling="no" allowtransparency="true" hspace="0" vspace="0"><img style="border:0px;max-width:100%;height:auto;" src="http://g.adspeed.net/ad.php?do=img&aid=243394&oid=19457&wd=121&ht=600&pair=as" width="121" height="600"/></iframe>
-	</noscript>
+		<!-- COMIENZO del código HTML de zanox-affiliate -->
+		<!-- ( El código HTML no debe cambiarse en pro de una funcionalidad correcta. ) -->
+		<a href="http://ad.zanox.com/ppc/?35233618C1503781728T"><img src="http://ad.zanox.com/ppv/?35233618C1503781728" align="bottom" border="0" hspace="1" alt="Aktion_120X600"></a>
+		<!--FIN del código HTML de zanox-affiliate -->
 	</div>
 	<!-- AdSpeed.com End -->
 </section>
