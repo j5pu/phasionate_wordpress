342513cf0929e840075e57af45d3f1fb4524d688
lobato02 <pablo@mnopi.com>
2015-10-15 12:58:37 +0200

    Cambios en banners de barra lateral, probando con sidebar.

--- wp-content/themes/kleo-child/page-parts/general-before-wrap.php
+++ wp-content/themes/kleo-child/page-parts/general-before-wrap.php
@@ -17,6 +17,13 @@ $container = apply_filters('kleo_main_container_class','container');
 ?>
 
 <section class="container-wrap main-color">
+	<!-- AdSpeed.com Tag 8.0.2 for [Ad] Swarovski - Laterales 120x600 -->
+	<div style="float: left; margin-right: 20px; width: 121px;">
+	<script type="text/javascript">var asdate=new Date();var q='&tz='+asdate.getTimezoneOffset()/60 +'&ck='+(navigator.cookieEnabled?'Y':'N') +'&jv='+(navigator.javaEnabled()?'Y':'N') +'&scr='+screen.width+'x'+screen.height+'x'+screen.colorDepth +'&z='+Math.random() +'&ref='+escape(document.referrer.substr(0,255)) +'&uri='+escape(document.URL.substr(0,255));document.write('<ifr'+'ame width="121" height="600" src="http://g.adspeed.net/ad.php?do=html&aid=243394&oid=19457&wd=121&ht=600&target=_blank'+q+'" frameborder="0" scrolling="no" allowtransparency="true" hspace="0" vspace="0"></ifr'+'ame>');</script>
+	<noscript><iframe width="121" height="600" src="http://g.adspeed.net/ad.php?do=html&aid=243394&oid=19457&wd=121&ht=600&target=_blank" frameborder="0" scrolling="no" allowtransparency="true" hspace="0" vspace="0"><img style="border:0px;max-width:100%;height:auto;" src="http://g.adspeed.net/ad.php?do=img&aid=243394&oid=19457&wd=121&ht=600&pair=as" width="121" height="600"/></iframe>
+	</noscript>
+	</div>
+	<!-- AdSpeed.com End -->
 	<div id="main-container" class="<?php echo $container; ?>">
 		<?php if($container == 'container') { ?><div class="row"> <?php } ?>
 
--- wp-content/themes/kleo-child/page-templates/full-width-sin-titulo-interstitial.php
+++ wp-content/themes/kleo-child/page-templates/full-width-sin-titulo-interstitial.php
@@ -25,14 +25,6 @@ kleo_switch_layout('right');
 
 <?php get_template_part('page-parts/general-before-wrap-no-title'); ?>
 
-<!-- AdSpeed.com Tag 8.0.2 for [Ad] Swarovski - Laterales 120x600 -->
-<div style="float: left; margin-right: 20px; width: 121px;">
-<script type="text/javascript">var asdate=new Date();var q='&tz='+asdate.getTimezoneOffset()/60 +'&ck='+(navigator.cookieEnabled?'Y':'N') +'&jv='+(navigator.javaEnabled()?'Y':'N') +'&scr='+screen.width+'x'+screen.height+'x'+screen.colorDepth +'&z='+Math.random() +'&ref='+escape(document.referrer.substr(0,255)) +'&uri='+escape(document.URL.substr(0,255));document.write('<ifr'+'ame width="121" height="600" src="http://g.adspeed.net/ad.php?do=html&aid=243394&oid=19457&wd=121&ht=600&target=_blank'+q+'" frameborder="0" scrolling="no" allowtransparency="true" hspace="0" vspace="0"></ifr'+'ame>');</script>
-<noscript><iframe width="121" height="600" src="http://g.adspeed.net/ad.php?do=html&aid=243394&oid=19457&wd=121&ht=600&target=_blank" frameborder="0" scrolling="no" allowtransparency="true" hspace="0" vspace="0"><img style="border:0px;max-width:100%;height:auto;" src="http://g.adspeed.net/ad.php?do=img&aid=243394&oid=19457&wd=121&ht=600&pair=as" width="121" height="600"/></iframe>
-</noscript>
-</div>
-<!-- AdSpeed.com End -->
-
 <?php
 if ( have_posts() ) :
     // Start the Loop.
@@ -58,14 +50,6 @@ if ( have_posts() ) :
 
 endif;
 ?>
-
-<!-- AdSpeed.com Tag 8.0.2 for [Ad] Swarovski - Laterales 120x600 -->
-<div style="float: right; margin-right: 20px; width: 121px;">
-<script type="text/javascript">var asdate=new Date();var q='&tz='+asdate.getTimezoneOffset()/60 +'&ck='+(navigator.cookieEnabled?'Y':'N') +'&jv='+(navigator.javaEnabled()?'Y':'N') +'&scr='+screen.width+'x'+screen.height+'x'+screen.colorDepth +'&z='+Math.random() +'&ref='+escape(document.referrer.substr(0,255)) +'&uri='+escape(document.URL.substr(0,255));document.write('<ifr'+'ame width="121" height="600" src="http://g.adspeed.net/ad.php?do=html&aid=243394&oid=19457&wd=121&ht=600&target=_blank'+q+'" frameborder="0" scrolling="no" allowtransparency="true" hspace="0" vspace="0"></ifr'+'ame>');</script>
-<noscript><iframe width="121" height="600" src="http://g.adspeed.net/ad.php?do=html&aid=243394&oid=19457&wd=121&ht=600&target=_blank" frameborder="0" scrolling="no" allowtransparency="true" hspace="0" vspace="0"><img style="border:0px;max-width:100%;height:auto;" src="http://g.adspeed.net/ad.php?do=img&aid=243394&oid=19457&wd=121&ht=600&pair=as" width="121" height="600"/></iframe>
-</noscript>
-</div>
-<!-- AdSpeed.com End -->
 
 <?php get_template_part('page-parts/general-after-wrap'); ?>
 
