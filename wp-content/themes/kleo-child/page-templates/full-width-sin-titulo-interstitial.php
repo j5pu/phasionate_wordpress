d3d467241dfb12ea01d57890d73e63337c506785
lobato02 <pablo@mnopi.com>
2015-10-15 12:22:45 +0200

    Cambios en el  banners en barras laterales (css)

--- wp-content/themes/kleo-child/page-templates/full-width-sin-titulo-interstitial.php
+++ wp-content/themes/kleo-child/page-templates/full-width-sin-titulo-interstitial.php
@@ -12,6 +12,12 @@
 
 get_header(); ?>
 
+<style type="text/css">
+article {
+    float: left;
+}
+</style>
+
 <?php
 //create full width template
 kleo_switch_layout('no');
@@ -53,7 +59,7 @@ endif;
 ?>
 
 <!-- AdSpeed.com Tag 8.0.2 for [Ad] Swarovski - Laterales 120x600 -->
-<div style="float: left; margin-right: 20px; width: 121px;">
+<div style="float: right; margin-right: 20px; width: 121px;">
 <script type="text/javascript">var asdate=new Date();var q='&tz='+asdate.getTimezoneOffset()/60 +'&ck='+(navigator.cookieEnabled?'Y':'N') +'&jv='+(navigator.javaEnabled()?'Y':'N') +'&scr='+screen.width+'x'+screen.height+'x'+screen.colorDepth +'&z='+Math.random() +'&ref='+escape(document.referrer.substr(0,255)) +'&uri='+escape(document.URL.substr(0,255));document.write('<ifr'+'ame width="120" height="600" src="'+(document.location.protocol=='https:'?'https://':'http://')+'g.adspeed.net/ad.php?do=html&aid=243394&oid=19457&wd=120&ht=600&target=_blank'+q+'" frameborder="0" scrolling="no" allowtransparency="true" hspace="0" vspace="0"></ifr'+'ame>');</script>
 <noscript><iframe width="120" height="600" src="//g.adspeed.net/ad.php?do=html&aid=243394&oid=19457&wd=120&ht=600&target=_blank" frameborder="0" scrolling="no" allowtransparency="true" hspace="0" vspace="0"><img style="border:0px;max-width:100%;height:auto;" src="//g.adspeed.net/ad.php?do=img&aid=243394&oid=19457&wd=120&ht=600&pair=as" width="120" height="600"/></iframe>
 </noscript>
