665d438a71e4ee8881bc373fdba7149e9a215d1d
lobato02 <pablo@mnopi.com>
2015-10-26 12:57:21 +0100

    Cambio de banners laterales, de Swarosky a Danone

--- wp-content/themes/kleo-child/page-parts/general-after-wrap.php
+++ wp-content/themes/kleo-child/page-parts/general-after-wrap.php
@@ -26,13 +26,11 @@ $container = apply_filters('kleo_main_container_class','container');
 			?>
 			<?php if($container == 'container') { ?></div><!--end .row--><?php } ?>
 		</div><!--end .container-->
-  	<!-- AdSpeed.com Tag 8.0.2 for [Ad] Swarovski - Laterales 120x600 -->
 	<div class="bannerright">
-		<!-- COMIENZO del código HTML de zanox-affiliate -->
-		<!-- ( El código HTML no debe cambiarse en pro de una funcionalidad correcta. ) -->
-		<a href="http://ad.zanox.com/ppc/?35233618C1503781728T"><img src="http://ad.zanox.com/ppv/?35233618C1503781728" align="bottom" border="0" hspace="1" alt="Aktion_120X600"></a>
-		<!--FIN del código HTML de zanox-affiliate -->
+	<!-- COMIENZO del código HTML de zanox-affiliate -->
+	<!-- ( El código HTML no debe cambiarse en pro de una funcionalidad correcta. ) -->
+	<a href="http://ad.zanox.com/ppc/?35378174C1551499395T"><img src="http://ad.zanox.com/ppv/?35378174C1551499395" align="bottom" border="0" hspace="1" alt="160X600-Vitalinea"></a>
+	<!--FIN del código HTML de zanox-affiliate -->
 	</div>
-	<!-- AdSpeed.com End -->
 </section>
 <!--END MAIN SECTION-->
