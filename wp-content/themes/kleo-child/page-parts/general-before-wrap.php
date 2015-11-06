5e6543b9e741a880daca9cc38070f445628b72af
lobato02 <pablo@mnopi.com>
2015-11-06 15:06:47 +0100

    Eliminados los banners de danone en los laterales y actualización de traducciones de WP Foto Vote

--- wp-content/themes/kleo-child/page-parts/general-before-wrap.php
+++ wp-content/themes/kleo-child/page-parts/general-before-wrap.php
@@ -17,13 +17,6 @@ $container = apply_filters('kleo_main_container_class','container');
 ?>
 
 <section class="container-wrap main-color">
-	<div class="bannerleft">
-	<!-- COMIENZO del código HTML de zanox-affiliate -->
-	<!-- ( El código HTML no debe cambiarse en pro de una funcionalidad correcta. ) -->
-	<a href="http://ad.zanox.com/ppc/?35378194C332490500T"><img src="http://ad.zanox.com/ppv/?35378194C332490500" align="bottom" border="0" hspace="1" alt="120X600-Vitalinea"></a>
-	<!--FIN del código HTML de zanox-affiliate -->
-	</div>
-	<!-- AdSpeed.com End -->
 	<div id="main-container" class="<?php echo $container; ?>">
 		<?php if($container == 'container') { ?><div class="row"> <?php } ?>
 
