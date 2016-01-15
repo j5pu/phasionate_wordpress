65aad806e12274ecb1a59231697a426927698b75
lobato02 <pablo@mnopi.com>
2016-01-15 13:42:44 +0100

    Cambios en plugin WP-foto-vote para añadir registro por facebook para votar

--- wp-content/plugins/wp-foto-vote/assets/js/fv_modal.js
+++ wp-content/plugins/wp-foto-vote/assets/js/fv_modal.js
@@ -143,6 +143,8 @@ var FvModal = {
 		this.openWidget("social-authorization");
 
 		this.showNotification("info", "", fv.lang.form_soc_msg, 0, 0);
+
+		do_action( 'bp_before_register_page' );
 	},
 	/*
 	 * Open modal with Title + message + Subscribe form
