3c2c6f352172bdaf8fcb842617de2ba3b08a50f2
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-03-11 18:24:58 +0100

    ajustes template ads laterales fijos. Interstitial solo aparece en movil

--- wp-content/plugins/boga-interstitial/boga-interstitial.php
+++ wp-content/plugins/boga-interstitial/boga-interstitial.php
@@ -5,7 +5,9 @@ Description: ¡Solo funciona con adrotate! Si se activa sin adrotate romperá wo
 */
 
 function insert_my_footer() {
-    include 'instertitial.php';
+    if (wp_is_mobile()){
+        include 'instertitial.php';
+    }
 }
 add_action('kleo_header', 'insert_my_footer');
 ?>
--- wp-content/themes/kleo-child/page-templates/right_siderbar_with_lateral_ads.php
+++ wp-content/themes/kleo-child/page-templates/right_siderbar_with_lateral_ads.php
@@ -29,21 +29,24 @@ get_header(); ?>
 <?php
 //create right sidebar template
 kleo_switch_layout('right');
-?>
 
-<!-- /61601326/u_lateral_derecho -->
-<div id='div-gpt-ad-1457625928117-1' style='height:600px; width:300px;position: fixed;right: 0;z-index: 0;margin-right: 2%;margin-top: 24px;'>
-    <script type='text/javascript'>
-        googletag.cmd.push(function() { googletag.display('div-gpt-ad-1457625928117-1'); });
-    </script>
-</div>
 
-<!-- /61601326/u_lateral_izquierdo -->
-<div id='div-gpt-ad-1457625928117-2' style='height:600px; width:300px; position: fixed; left: 0; margin-left: 2%;margin-top: 24px;'>
-    <script type='text/javascript'>
-        googletag.cmd.push(function() { googletag.display('div-gpt-ad-1457625928117-2'); });
-    </script>
-</div>
+if (!wp_is_mobile()){ ?>
+    <!-- /61601326/u_lateral_derecho -->
+    <div id='div-gpt-ad-1457625928117-1' style='height:600px; width:300px;position: fixed;right: 0;z-index: 0;margin-top: 24px;'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1457625928117-1'); });
+        </script>
+    </div>
+
+    <!-- /61601326/u_lateral_izquierdo -->
+    <div id='div-gpt-ad-1457625928117-2' style='height:600px; width:300px; position: fixed; left: 0;margin-top: 24px;'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1457625928117-2'); });
+        </script>
+    </div>
+<?php } ?>
+
 
 <?php get_template_part('page-parts/general-title-section'); ?>
 
