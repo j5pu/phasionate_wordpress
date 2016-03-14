a5b4b8528fde3e9212ca58ca0bfe48ba38932601
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-03-14 20:53:36 +0100

    fix cabecera ad (faltaba div)

--- wp-content/themes/kleo-child/header.php
+++ wp-content/themes/kleo-child/header.php
@@ -84,7 +84,7 @@
 				jQuery("#div-gpt-ad-1457625928117-2").hide();
 			}
 			if (width >= 999 && width <= 1237) {
-				googletag.defineSlot('/61601326/cabecera', [970, 90], 'div-gpt-ad-1457975907235-0').addService(googletag.pubads());
+				googletag.defineSlot('/61601326/cabecera', [970, 90], 'div-gpt-ad-1457985102265-0').addService(googletag.pubads());
 			}
 			if (width <= 989){
 				googletag.defineSlot('/61601326/mayor', [300, 600], 'div-gpt-ad-1457972362060-0').addService(googletag.pubads());
@@ -138,6 +138,12 @@ $site_style = apply_filters('kleo_site_style', $site_style);
 	?>
 
 	<?php if (!wp_is_mobile()){ ?>
+	<!-- /61601326/cabecera -->
+	<div id='div-gpt-ad-1457985102265-0' style='height:90px; width:970px; top: 0; margin: 0 auto;'>
+		<script type='text/javascript'>
+			googletag.cmd.push(function() { googletag.display('div-gpt-ad-1457985102265-0'); });
+		</script>
+	</div>
 	<!-- /61601326/u_lateral_derecho -->
 	<div id='div-gpt-ad-1457625928117-1' style='height:600px; width:300px;position: fixed;right: 0;z-index: 0;margin-top: 24px;'>
 		<script type='text/javascript'>
