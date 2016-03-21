f20532616d02cc96c7cd068f2df08a52070543ee
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-03-21 17:43:12 +0100

    bp redirect after register

--- wp-content/themes/kleo-child/functions.php
+++ wp-content/themes/kleo-child/functions.php
@@ -1030,6 +1030,12 @@ function subscriber_login_redirect( $redirect_to, $request, $user  ) {
 }
 add_filter( 'login_redirect', 'subscriber_login_redirect', 10, 3 );
 
+
+function my_signup_redirect( $user_id, $user_login, $user_password, $user_email, $usermeta ) {
+	wp_redirect( home_url() . '/concurso-portada' );
+	exit();
+}
+add_action( 'bp_core_signup_user', 'my_signup_redirect', 10, 5 );
 /*
 
 -----BORRA ESTE COMENTARIO UNA VEZ HECHOS ESTOS PASOS
