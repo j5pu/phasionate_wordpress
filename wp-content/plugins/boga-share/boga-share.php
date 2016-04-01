10f6fc2e67c2fd0c4014f6332c1cd8bd8ce77a07
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-04-01 17:50:46 +0200

    bug fix nombre función repetido

--- wp-content/plugins/boga-share/boga-share.php
+++ wp-content/plugins/boga-share/boga-share.php
@@ -4,10 +4,10 @@ Plugin Name: BogaShare
 Description: Muestra el cajon de compartir a traves de api para el concurso de share
 */
 
-function insert_my_footer() {
+function show_bogashare_dialog() {
     if(is_single(11826)){
         include 'share.php';
     }
 }
-add_action('wp_footer', 'insert_my_footer');
+add_action('wp_footer', 'show_bogashare_dialog');
 ?>
